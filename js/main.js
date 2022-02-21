2ecbf8476a9bf6d8c65c928b3d20da742fa9bf58
meduza <lmdogg@mail.ru>
2022-02-20 22:24:27 -0800

    Вторая функция

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const getRandomInteger = (minimalInteger, maximalInteger) => {
-  minimalInteger = Math.ceil(minimalInteger);
-  maximalInteger = Math.floor(maximalInteger);
 
-  if (minimalInteger < 0) {
+const getRandomInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (min < 0) {
     return 'Неприемлимо отрицательное значение';
   }
 
-  if (maximalInteger <= minimalInteger) {
-    return 'Максимальное значение не должно быть меньше или быть равно минимальному значению';
+  if (max <= min) {
+    return 'Максимальное значение не должно быть меньше или равно минимальному';
   }
 
-  return Math.floor(Math.random() * (maximalInteger - minimalInteger + 1)) + minimalInteger;
-
-  /*
-  // На доработку (в будущем)
-  // Избавиться от ошибки "Do not nest ternary expressions no-nested-ternary", попытаться в одну строку
-  return (minimalInteger < 0)
-    ? 'Неприемлимо отрицательное значение'
-    : (maximalInteger <= minimalInteger)
-      ? 'Максимальное значение не должно быть меньше или быть равно минимальному значению'
-      : Math.floor(Math.random() * (maximalInteger - minimalInteger + 1)) + minimalInteger;
-  */
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 getRandomInteger(1, 5);
+
+// https://learn.javascript.ru/number
+const getRandomFloat = (min, max, amountOfSymbolsAfterComma) => {
+
+  if (min < 0) {
+    return 'Неприемлимо отрицательное значение';
+  }
+
+  if (max <= min) {
+    return 'Максимальное значение не должно быть меньше или равно минимальному';
+  }
+
+  const random = Math.random() * (max - min) + min;
+  const randomToString = random.toFixed(amountOfSymbolsAfterComma);
+  return +randomToString;
+};
+
+getRandomFloat(1, 5, 5);

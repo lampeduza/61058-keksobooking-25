335fc5343f4d0244f6a4c1bca1ddb3917d980043
meduza <lmdogg@mail.ru>
2022-02-23 01:06:28 -0800

    Переиспользовал код функций

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const generateNumber = (min, max) => Math.random() * (max - min + 1) + min;
+
+const generateError = (min, max) => {
+  if (min < 0) {
+    throw new RangeError('Неприемлимо отрицательное значение');
+  }
+
+  if (max <= min) {
+    throw new RangeError('Максимальное значение не должно быть меньше или равно минимальному');
+  }
+};
+
 const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (min < 0) {
-    return 'Неприемлимо отрицательное значение';
-  }
+  generateError(min, max);
 
-  if (max <= min) {
-    return 'Максимальное значение не должно быть меньше или равно минимальному';
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(generateNumber(min, max));
 };
 
 getRandomInteger(1, 5);
 
 // https://learn.javascript.ru/number
 const getRandomFloat = (min, max, amountOfSymbolsAfterComma) => {
+  generateError(min, max);
 
-  if (min < 0) {
-    return 'Неприемлимо отрицательное значение';
-  }
-
-  if (max <= min) {
-    return 'Максимальное значение не должно быть меньше или равно минимальному';
-  }
-
-  const random = Math.random() * (max - min) + min;
+  const random = generateNumber(min, max);
   const randomToString = random.toFixed(amountOfSymbolsAfterComma);
   return +randomToString;
 };

1f818046362bfdfa618663f251a0dfa979c3bfb5
meduza <lmdogg@mail.ru>
2022-02-28 02:37:43 -0800

    Написал комментарии

--- js/main.js
+++ js/main.js
@@ -51,6 +51,7 @@ const TIMES = [
   '14:00'
 ];
 
+// FEATURES НУЖНО КАК ТО ЗАПИСЫВАТЬ В НОВЫЙ МАССИВ, БЕЗ ПОВТОРЕНИЙ, НЕ ПОНИМАЮ, КАК..
 const FEATURES = [
   'wifi',
   'dishwasher',
@@ -67,14 +68,19 @@ const DESCRIPTIONS = [
   'В подъезде имеется консьерж. Ведётся видеонаблюдение'
 ];
 
+// PHOTOS - ТОЖЕ САМОЕ, НУЖНО ЗАПИСЫВАТЬ В НОВЫЙ МАССИВ
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
+// Не понимаю вот эту функцию до конца. То ли параметр глупый, то ли я бы сменил параметр на параметр типа array.
+// array => array[getRandomFloat(0, array.length - 1)]; - так кажется очевиднее. В демонстрации написано elements, why?
 const getRandomArrayElement = (elements) => elements[getRandomFloat(0, elements.length - 1)];
 
+// generateAvatar (получилось странное решение, но эта функция хотя бы генерирует правильно элементы массива
+// не понимаю, как доработать до такого состояния, когда в возвращаемом объекте будут пути, которые не будут повторяться. 
 const generateAvatar = () => {
   let array = [];
   let path = '';
@@ -87,9 +93,8 @@ const generateAvatar = () => {
   return getRandomArrayElement(array);
 };
 
-generateAvatar();
-
 const generateAd = () => {
+  // Как обойтись без этого объекта? А только передавать location.lat and location.lng. Наверное это области видимости..
   const locationData = {
     x: getRandomFloat(35.65000, 35.70000, 5),
     y: getRandomFloat(139.70000, 139.80000, 5)
@@ -112,6 +117,8 @@ const generateAd = () => {
       description: getRandomArrayElement(DESCRIPTIONS),
       photos: PHOTOS, // ['array', 'of', 'strings']
     },
+    // в location.lat and location.lng вызывал функцию getRandomFloat().
+    // Вроде все работает, но, когда в address передаю location.lat and location.lng - выводится undefine? Это области видимости?
     location: {
       lat: locationData.x,
       lng: locationData.y

55e10326c782fe19a39c3bf7209284db57a58e22
meduza <lmdogg@mail.ru>
2022-02-27 19:45:54 -0800

    Все, кроме avatar, features, photos

--- js/main.js
+++ js/main.js
@@ -19,8 +19,90 @@ const getRandomFloat = (min, max, digits = 0) => {
 
   checkArguments(min, max);
 
-  const randomNumber = Math.random() * (max - min + 1) + min;
+  const randomNumber = Math.random() * (max - min) + min;
   return numberToFixed(randomNumber, digits);
 };
 
-getRandomFloat(1, 5, 5);
+/*
+  Генерация данных
+*/
+const TITLES = [
+  'Сдам уютную студию!',
+  '5 минут пешком до метро!',
+  'Топ предложение в район!е',
+  'Успейте снять по старой цене!',
+  'Окна на парк!',
+  'Жк бизнес класса!',
+  'Без комиссии!',
+  'Заезжай и живи!'
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const DESCRIPTIONS = [
+  'Квартира супер атмосферная, подойдет для парочки',
+  'Квартира уютная, чистая и просторная',
+  'Есть все необходимое для комфортного проживания',
+  'В подъезде имеется консьерж. Ведётся видеонаблюдение'
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const getRandomArrayElement = (arrayElements) => arrayElements[getRandomFloat(0, arrayElements.length - 1)];
+
+const generateAd = () => {
+  const locations = {
+    x: getRandomFloat(35.65000, 35.70000, 5),
+    y: getRandomFloat(139.70000, 139.80000, 5)
+  };
+
+  return {
+    author: {
+      avatar: 1
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: locations.x + ' ' + locations.y,
+      price: getRandomFloat(30000, 120000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomFloat(1, 10),
+      guests: getRandomFloat(1, 10),
+      checking: getRandomArrayElement(TIMES),
+      checkout: getRandomArrayElement(TIMES),
+      features: FEATURES, // ['array', 'of', 'strings']
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: PHOTOS, // ['array', 'of', 'strings']
+    },
+    location: {
+      lat: locations.x,
+      lng: locations.y
+    }
+  };
+};
+
+console.log(generateAd());

48c9e0448c934ce386da3f23736d1aa1ba1c4b75
meduza <lmdogg@mail.ru>
2022-03-13 00:23:40 -0800

    Вынес функции на уровень выше

--- js/ads.js
+++ js/ads.js
@@ -13,96 +13,94 @@ const adTemplate = document.querySelector('#card')
 
 const similarAds = getObjectsArray();
 
-// Создал document fragment
-const documentFragment = document.createDocumentFragment();
+// Создал document fragment для adFeatures
+const featuresListFragment = document.createDocumentFragment();
+
+const adFeatures = (ad, similarAd) => {
+  const adFeaturesList = ad.querySelector('.popup__features');
+
+  while (adFeaturesList.firstChild) {
+    adFeaturesList.firstChild.remove();
+  }
+
+  for (let j = 0; j <= similarAd.offer.features.length - 1; j++) {
+    const adFeaturesElement = document.createElement('li');
+    adFeaturesElement.classList.add('popup__feature', `popup__feature--${similarAd.offer.features[j]}`);
+    const clonedElement = adFeaturesElement.cloneNode(true);
+    featuresListFragment.appendChild(clonedElement);
+  }
+
+  adFeaturesList.appendChild(featuresListFragment);
+};
+
+const switchEngToRus = (ad, similarAd) => {
+  const russianDictionary = {
+    palace: 'Дворец',
+    flat: 'Квартира',
+    house: 'Дом',
+    bungalow: 'Бунгало',
+    hotel: 'Отель'
+  };
+
+  return russianDictionary[similarAd.offer.type];
+};
+
+const getRightRoomsPronunciation = (ad, similarAd) => {
+  if (similarAd.offer.rooms === 1) {
+    return 'комната';
+  } else if (similarAd.offer.rooms <= 4) {
+    return 'комнаты';
+  } else {
+    return 'комнат';
+  }
+};
+
+const getRightGuestsPronunciation = (ad, similarAd) => {
+  if (similarAd.offer.guests === 1) {
+    return 'гостя';
+  } else if (similarAd.offer.guests > 1) {
+    return 'гостей';
+  }
+};
+
+// Создал document fragment для createImages
+const imagesListFragment = document.createDocumentFragment();
+
+const createImages = (ad, similarAd) => {
+  const adPhotosList = ad.querySelector('.popup__photos');
+  const adPhotosImage = adPhotosList.querySelector('.popup__photo');
+
+  for (let j = 0; j <= similarAd.offer.photos.length - 1; j++) {
+    const clonedImage = adPhotosImage.cloneNode(true);
+    clonedImage.src = similarAd.offer.photos[j];
+    imagesListFragment.appendChild(clonedImage);
+  }
+
+  adPhotosImage.remove();
+  adPhotosList.appendChild(imagesListFragment);
+};
+
+// Создал document fragment для generateAds
+const adsListFragment = document.createDocumentFragment();
 
 const generateAds = () => {
   const ad = adTemplate.cloneNode(true);
-
-  //for (let i = 0; i <= similarAds.length - 1; i++) {
+  
   for (let similarAd of similarAds) {
-
-    // Понять, как вынести эту функцию в глобальную область видимости
-    const switchEngToRus = () => {
-      if (similarAd.offer.type === 'palace') {
-        return 'Дворец';
-      } else if (similarAd.offer.type === 'flat') {
-        return 'Квартира';
-      } else if (similarAd.offer.type === 'house') {
-        return 'Дом';
-      } else if (similarAd.offer.type === 'bungalow') {
-        return 'Бунгало';
-      } else {
-        return 'Отель';
-      }
-    };
-
-    // Понять, как вынести эту функцию в глобальную область видимости
-    const getRightRoomsPronunciation = () => {
-      if (similarAd.offer.rooms === 1) {
-        return 'комната';
-      } else if (similarAd.offer.rooms <= 4) {
-        return 'комнаты';
-      } else {
-        return 'комнат';
-      }
-    };
-
-    // Понять, как вынести эту функцию в глобальную область видимости
-    const getRightGuestsPronunciation = () => {
-      if (similarAd.offer.guests === 1) {
-        return 'гостя';
-      } else if (similarAd.offer.guests > 1) {
-        return 'гостей';
-      }
-    };
-
-    // Понять, как вынести эту функцию в глобальную область видимости
-    const adFeatures = () => {
-      const adFeaturesList = ad.querySelector('.popup__features');
-
-      // Удаляю элементы списка
-      for (let i = adFeaturesList.children.length - 1; i >= 0; i--) {
-        const child = adFeaturesList.children[i];
-        child.parentElement.removeChild(child);
-      }
-
-      for (let j = 0; j <= similarAd.offer.features.length - 1; j++) {
-        const adFeaturesElement = document.createElement('li');
-        adFeaturesElement.classList.add('popup__feature', `popup__feature--${similarAd.offer.features[j]}`);
-        const clonedElement = adFeaturesElement.cloneNode(true);
-        adFeaturesList.appendChild(clonedElement);
-      }
-    };
-
-    // Понять, как вынести эту функцию в глобальную область видимости
-    const createImages = () => {
-      const adPhotosList = ad.querySelector('.popup__photos');
-      const adPhotosImage = adPhotosList.querySelector('.popup__photo');
-
-      for (let j = 0; j <= similarAd.offer.photos.length - 1; j++) {
-        const clonedImage = adPhotosImage.cloneNode(true);
-        clonedImage.src = similarAd.offer.photos[j];
-        adPhotosList.appendChild(clonedImage);
-      }
-
-      adPhotosImage.remove();
-    };
-
     ad.querySelector('.popup__title').textContent = similarAd.offer.title;
     ad.querySelector('.popup__text--address').textContent = similarAd.offer.address;
     ad.querySelector('.popup__text--price').innerHTML = `${similarAd.offer.price} <span>₽/ночь</span>`;
-    ad.querySelector('.popup__type').textContent = switchEngToRus();
-    ad.querySelector('.popup__text--capacity').textContent = `${similarAd.offer.rooms}  ${getRightRoomsPronunciation()} для ${similarAd.offer.guests} ${getRightGuestsPronunciation()}`;
+    ad.querySelector('.popup__type').textContent = switchEngToRus(ad, similarAd);
+    ad.querySelector('.popup__text--capacity').textContent = `${similarAd.offer.rooms}  ${getRightRoomsPronunciation(ad, similarAd)} для ${similarAd.offer.guests} ${getRightGuestsPronunciation(ad, similarAd)}`;
     ad.querySelector('.popup__text--time').textContent = `Заезд после ${similarAd.offer.checking} , выезд до ${similarAd.offer.checkout}`;
     ad.querySelector('.popup__description').textContent = similarAd.offer.description;
     ad.querySelector('.popup__avatar').src = similarAd.author.avatar;
-    adFeatures();
-    createImages();
+    adFeatures(ad, similarAd);
+    createImages(ad, similarAd);
   }
 
-  adsList.appendChild(ad);
-  return adsList;
+  adsListFragment.appendChild(ad);
+  adsList.appendChild(adsListFragment);
 };
 
 export {generateAds};

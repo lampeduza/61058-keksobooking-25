4e1ea84535d9144ccc36bcc62194f33e161d8611
meduza <lmdogg@mail.ru>
2022-03-13 22:46:44 -0700

    disableInterface, enableInterface functions

---
+++ js/form.js
@@ -0,0 +1,46 @@
+// В задаче пишут, что (все пункты должны быть заблокированы, кроме первого, про карту) как это понимать?
+// DISABLE INTERFACE
+const disableInterface = () => {
+	// filter form
+	const filtersForm = document.querySelector('.map__filters');
+	filtersForm.classList.add('map__filters--disabled');
+
+	const filterFormElements = filtersForm.querySelector('.map__features');
+	filterFormElements.disabled = true;
+
+	// ad form
+	const adForm = document.querySelector('.ad-form');
+	adForm.classList.add('ad-form--disabled');
+
+	const adFormElements = adForm.querySelectorAll('fieldset');
+	adFormElements.forEach(element => element.disabled = true);
+
+	// Как можно заблокировать обычный div? Это же div. Я так понял, класс сюда disabled не нужно добавлять.
+	// Если по сути, то я родителя заблокировал, зачем какой - то контейнер блокировать?
+	// А еще, хоть я и заблокировал div, но в разметке этого атрибута нет.. Я полагаю, что у div'a нету этой фишки, focus, по этому и нет атрибута в DOM?
+	// Его браузер не отображат, хотя, когда я вывожу его в консоль, он выводится как объект, я его открываю и там написано slider disabled: true. что?))
+	const slider = adForm.querySelector('.ad-form__slider');
+	slider.classList.add('ad-form__slider--disabled');
+	slider.disabled = true;
+}
+
+// ENABLE INTERFACE
+const enableInterface = () => {
+	const filtersForm = document.querySelector('.map__filters');
+	filtersForm.classList.remove('map__filters--disabled');
+
+	const filterFormElements = filtersForm.querySelector('.map__features');
+	filterFormElements.disabled = false;
+
+	const adForm = document.querySelector('.ad-form');
+	adForm.classList.remove('ad-form--disabled');
+
+	const adFormElements = adForm.querySelectorAll('fieldset');
+	adFormElements.forEach(element => element.disabled = false);
+
+	const slider = adForm.querySelector('.ad-form__slider');
+	slider.classList.remove('ad-form__slider--disabled');
+	slider.disabled = false;
+};
+
+export {disableInterface, enableInterface};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 import {generateAds, similarAds} from './ads.js';
+import {disableInterface, enableInterface} from './form.js';
 
 // Вызов ads.js
 generateAds(similarAds);
+
+// Вызов form.js
+disableInterface();
+//enableInterface();

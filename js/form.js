1929209d749ee40ededa8057b1990c7876e7a179
meduza <lmdogg@mail.ru>
2022-03-14 07:22:39 -0700

    Задание

--- js/form.js
+++ js/form.js
@@ -1,50 +1,60 @@
 // В задаче пишут, что (все пункты должны быть заблокированы, кроме первого, про карту) как это понимать?
 // DISABLE INTERFACE
 const disableInterface = () => {
-	// filter form
-	const filtersForm = document.querySelector('.map__filters');
-	filtersForm.classList.add('map__filters--disabled');
+  // filter form
+  const filtersForm = document.querySelector('.map__filters');
+  filtersForm.classList.add('map__filters--disabled');
 
-	const filterFormElements = filtersForm.querySelector('.map__features');
-	filterFormElements.disabled = true;
+  const filterFormElements = filtersForm.querySelector('.map__features');
+  filterFormElements.disabled = true;
 
-	// ad form
-	const adForm = document.querySelector('.ad-form');
-	adForm.classList.add('ad-form--disabled');
+  // ad form
+  const adForm = document.querySelector('.ad-form');
+  adForm.classList.add('ad-form--disabled');
 
-	const adFormElements = adForm.querySelectorAll('fieldset');
-	adFormElements.forEach(element => element.disabled = true);
+  const adFormElements = adForm.querySelectorAll('fieldset');
 
-	// Как можно заблокировать обычный div? Это же div. Я так понял, класс сюда disabled не нужно добавлять.
-	// Если по сути, то я родителя заблокировал, зачем какой - то контейнер блокировать?
-	// А еще, хоть я и заблокировал div, но в разметке этого атрибута нет.. Я полагаю, что у div'a нету этой фишки, focus, по этому и нет атрибута в DOM?
-	// Его браузер не отображат, хотя, когда я вывожу его в консоль, он выводится как объект, я его открываю и там написано slider disabled: true. что?))
-	const slider = adForm.querySelector('.ad-form__slider');
-	slider.classList.add('ad-form__slider--disabled');
-	slider.disabled = true;
-}
+  // Почему нельзя использовать вот такой метод? Пишет "Arrow function should not return assignment no-return-assign"
+  // adFormElements.forEach((element) => element.disabled = false);
+  /* А вот этот метод менее красив, но его можно
+  adFormElements.forEach((element) => {
+    element.disabled = false;
+  });
+  */
 
-disableInterface();
+  for (const adFormElement of adFormElements) {
+    adFormElement.disabled = true;
+  }
+
+  // Как можно заблокировать обычный div? Это же div. Я так понял, класс сюда disabled не нужно добавлять.
+  // Если по сути, то я родителя заблокировал, зачем какой - то контейнер блокировать?
+  // А еще, хоть я и заблокировал div, но в разметке этого атрибута нет.. Я полагаю, что у div'a нету этой фишки, focus, по этому и нет атрибута в DOM?
+  // Его браузер не отображат, хотя, когда я вывожу его в консоль, он выводится как объект, я его открываю и там написано slider disabled: true. что?))
+  const slider = adForm.querySelector('.ad-form__slider');
+  slider.classList.add('ad-form__slider--disabled');
+  slider.disabled = true;
+};
 
 // ENABLE INTERFACE
 const enableInterface = () => {
-	const filtersForm = document.querySelector('.map__filters');
-	filtersForm.classList.remove('map__filters--disabled');
+  const filtersForm = document.querySelector('.map__filters');
+  filtersForm.classList.remove('map__filters--disabled');
 
-	const filterFormElements = filtersForm.querySelector('.map__features');
-	filterFormElements.disabled = false;
+  const filterFormElements = filtersForm.querySelector('.map__features');
+  filterFormElements.disabled = false;
 
-	const adForm = document.querySelector('.ad-form');
-	adForm.classList.remove('ad-form--disabled');
+  const adForm = document.querySelector('.ad-form');
+  adForm.classList.remove('ad-form--disabled');
 
-	const adFormElements = adForm.querySelectorAll('fieldset');
-	adFormElements.forEach(element => element.disabled = false);
+  const adFormElements = adForm.querySelectorAll('fieldset');
 
-	const slider = adForm.querySelector('.ad-form__slider');
-	slider.classList.remove('ad-form__slider--disabled');
-	slider.disabled = false;
+  for (const adFormElement of adFormElements) {
+    adFormElement.disabled = false;
+  }
+
+  const slider = adForm.querySelector('.ad-form__slider');
+  slider.classList.remove('ad-form__slider--disabled');
+  slider.disabled = false;
 };
-
-enableInterface();
 
 export {disableInterface, enableInterface};

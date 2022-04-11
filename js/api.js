1bf1f7ddd9e2d12fa89582744c31dc2944b7d265
meduza <lmdogg@mail.ru>
2022-04-11 19:51:25 +0700

    Написал обработчики

--- js/api.js
+++ js/api.js
@@ -22,11 +22,6 @@ const errorTemplate = document.querySelector('#error')
   .content
   .querySelector('.error');
 
-const tryAgainButton = errorTemplate.querySelector('.error__button');
-
-const showMessage = (template) => document.body.append(template);
-
-
 const sendData = () => {
   fetch('https://25.javascript.pages.academy/keksobooking',
     {
@@ -38,11 +33,7 @@ const sendData = () => {
 
     .then((response) => {
       if (response.ok) {
-        // Показать попап об успешной отправке данных
-        // Сообщение должно исчезать по нажатию на клавишу Esc и по клику на произвольную область экрана (window)
-        // Нужно удалять обработчики
-        showMessage(successTemplate);
-        closeMessage(successTemplate);
+        showSuccessMessage();
         console.log('show successful message');
         return;
       }
@@ -50,40 +41,69 @@ const sendData = () => {
       throw new Error(`${response.status} ${response.statusText}`);
     })
     .catch((err) => {
-      // Показать попап об ошибке отправке данных
-      // Сообщение должно исчезать после нажатия на .error__button,
-      // по нажатию на Esc и по клику на произвольную область экрана (window)
-      // + сохранить данные для повторной отправки формы
-      showMessage(errorTemplate);
-      closeMessage(errorTemplate);
+      // сохранить данные для повторной отправки формы
+      showErrorMessage();
       console.log(err);
     });
 };
 
-
-/* listeners' callbacks */
-const onDocumentClick = (template) => {
-  template.remove();
-}
-
-const onEscKeydown = (template, evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    template.remove();
-  }
-}
-/* end listeners' callbacks */
-
-
-const closeMessage = (template) => {
-  document.addEventListener('click', () => {
-    onDocumentClick(template);
-  });
-
-  document.addEventListener('keydown', (evt) => {
-    onEscKeydown(template, evt);
-  });
+const closeSuccessMessage = () => {
+  successTemplate.remove();
+  window.removeEventListener('click', onSuccessWindowClick);
+  window.removeEventListener('keydown', onSuccessWindowKeydown);
 };
 
+const onSuccessWindowClick = () => {
+  closeSuccessMessage();
+};
+
+const onSuccessWindowKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    closeSuccessMessage();
+  }
+};
+
+const closeErrorMessage = () => {
+  errorTemplate.remove();
+  window.removeEventListener('click', onErrorWindowClick);
+  window.removeEventListener('keydown', onErrorWindowKeydown);
+  button.removeEventListener('click', onButtonClick);
+};
+
+const onErrorWindowClick = () => {
+  closeErrorMessage();
+};
+
+const onErrorWindowKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    closeErrorMessage();
+  }
+};
+
+const onButtonClick = () => {
+  closeErrorMessage();
+};
+
+const showSuccessMessage = () => {
+  document.body.append(successTemplate);
+
+  window.addEventListener('click', onSuccessWindowClick);
+  window.addEventListener('keydown', onSuccessWindowKeydown);
+};
+
+const showErrorMessage = () => {
+  document.body.append(errorTemplate);
+
+  window.addEventListener('click', onErrorWindowClick);
+  window.addEventListener('keydown', onErrorWindowKeydown);
+
+  const button = errorTemplate.querySelector('.error__button');
+  
+  if (!button) {
+    return;
+  }
+
+  button.addEventListener('click', onButtonClick);
+};
 
 export {getData, sendData};

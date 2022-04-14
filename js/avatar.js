fad9510dba68f004f0315cd02f98450f9b2faf45
meduza <lmdogg@mail.ru>
2022-04-14 18:07:42 +0700

    Обернул в функцию

--- js/avatar.js
+++ js/avatar.js
@@ -2,18 +2,21 @@ import {adForm} from './validation.js';
 import {FILE_TYPES} from './data.js';
 
 // Выбор Аватарки (загрузка аватарки)
-const imageChooser = adForm.querySelector('input[name="avatar"]');
-const previewArea = adForm.querySelector('.ad-form-header__preview img');
+const chooseAvatar = () => {
+	const imageChooser = adForm.querySelector('input[name="avatar"]');
+	const previewArea = adForm.querySelector('.ad-form-header__preview img');
 
-imageChooser.addEventListener('change', () => {
-	const image = imageChooser.files[0];
-	const imageName = image.name.toLowerCase();
+	imageChooser.addEventListener('change', () => {
+		const image = imageChooser.files[0];
+		const imageName = image.name.toLowerCase();
 
-	const matches = FILE_TYPES.some((it) => imageName.endsWith(it));
+		const matches = FILE_TYPES.some((it) => imageName.endsWith(it));
 
-	if (matches) {
-		previewArea.src = URL.createObjectURL(image);
-	}
-});
+		if (matches) {
+			previewArea.src = URL.createObjectURL(image);
+		}
+	});
+};
 
 // Выбор фотографии жилья (загрузка фотография жилья)
+export {chooseAvatar};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {setupMap} from './map.js';
 import {PIN_COUNT} from './data.js';
 import {getData} from './api.js';
-import './avatar.js';
+import {chooseAvatar} from './avatar.js';
 
 getData((data) => setupMap(data.slice(0, PIN_COUNT)));
+chooseAvatar();
